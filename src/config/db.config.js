fe419aad18ddf78d041cf339edfe16026ce2192c
leanhvu21042001 <leanhvu21042001@gmail.com>
2022-03-14 15:39:02 +0700

    add config db.config

---
+++ src/config/db.config.js
@@ -0,0 +1,7 @@
+export default {
+	DATABASE_NAME: 'dbmanager',
+	USER_NAME: 'root',
+	PASSWORD: '',
+	HOST: 'localhost',
+	DIALECT: 'mysql'
+};
--- src/models/database.js
+++ src/models/database.js
@@ -1,9 +1,15 @@
 import { Sequelize } from 'sequelize';
+import dbConfig from '../config/db.config';
 
-const db = new Sequelize('dbmanager', 'root', '', {
-	host: 'localhost',
-	dialect: 'mysql'
-});
+const db = new Sequelize(
+	dbConfig.DATABASE_NAME,
+	dbConfig.USER_NAME,
+	dbConfig.PASSWORD,
+	{
+		host: dbConfig.HOST,
+		dialect: dbConfig.DIALECT
+	}
+);
 
 db.authenticate().then(() => console.log('co so du lieu da duoc ket noi !!!'));
 
